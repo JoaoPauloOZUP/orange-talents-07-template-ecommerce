d0b19d66f9de215ee71cc722fe981800554438e2
Joao Paulo Oliveira <joao.lira@zup.com.br>
2021-08-16 17:36:16 -0300

    Retirando a API pública a todos

--- src/main/java/br/com/zupacademy/joao/mercadolivre/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/zupacademy/joao/mercadolivre/config/security/SecurityConfiguration.java
@@ -45,7 +45,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/usuario").permitAll()
                 .antMatchers(HttpMethod.POST, "/atenticacao").permitAll()
-                .antMatchers(HttpMethod.POST, "/**").permitAll()
                 .anyRequest().authenticated()
                 .and().cors()
                 .and().csrf().disable()
